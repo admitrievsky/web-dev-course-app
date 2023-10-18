4c0bfea09820df516b53aa70af2d0957eca77942
Anton Dmitrievsky <admitrievsky@gmail.com>
2023-10-18 22:11:47 +0500

    [product_list]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import request, render_template, Flask
 
 app = Flask(__name__)
 
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
-    return render_template('index.html', name=name)
+    product_list = [
+        {'name': 'iPhone', 'price': 999.99},
+        {'name': 'Samsung Galaxy', 'price': 899.99},
+        {'name': 'Google Pixel', 'price': 699.99}
+    ]
+    return render_template('index.html', name=name, products=product_list)
 
 
 if __name__ == '__main__':
