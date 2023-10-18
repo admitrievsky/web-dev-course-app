fed5de66423217720ba09cc561907741c488d669
Anton Dmitrievsky <admitrievsky@gmail.com>
2023-10-18 22:11:02 +0500

    pass a query parameter to the template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    name = request.args.get('name', 'World')
+    return render_template('index.html', name=name)
 
 
 if __name__ == "__main__":
