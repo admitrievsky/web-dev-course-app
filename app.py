1f87ed41e49d00a368d636a48eaff12d2c9db7fa
Anton Dmitrievsky <admitrievsky@gmail.com>
2023-10-18 22:11:47 +0500

    add a product list stub

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
-    return render_template('index.html', name=name)
+    product_list = [
+        {'name': 'iPhone', 'price': 999.99},
+        {'name': 'Samsung Galaxy', 'price': 899.99},
+        {'name': 'Google Pixel', 'price': 699.99},
+    ]
+
+    return render_template('index.html', name=name, products=product_list)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
